6367a4afe4ac55bc36ee8c1966692c21f5930c1d
tomasbarcellos <tomasbarcellos@gmail.com>
2017-01-16 19:13:39 -0200

    Atualizações das funções e do README

--- R/CEPAL_stat.R
+++ R/CEPAL_stat.R
@@ -2,15 +2,26 @@
 #'
 #' Esta função faz download das séries da CEPAL
 #' @param id_serie ID da série que será baixada. Para conhecer as séries disponíveis e seus IDs, use a função series_CEPAL().
+#' @param exportar Os dados devem ser exportados? Neste caso o nome de um arquivo deve ser fornecido e o retorno será NULL. Padrão: FALSE
+#' @param arquivo O nome do arquivo que será criado caso exportar = TRUE. Padrão: ""
+#' @param silencioso O progresso deve ser informado? Padrão: FALSE
 #' @keywords CEPAL dados
 #' @export
 #' @examples
 #' termos_troca <- CEPAL_stat(883)
 
-CEPAL_stat <- function(id_serie) {
-  if(!require(rvest) |
-     !require(tidyr) |
-     !require(dplyr)) stop("Precisa dos pacotes 'rvest', 'tidyr' e 'dplyr'.")
+CEPAL_stat <- function(id_serie, exportar = FALSE,
+                       arquivo = "", silencioso = FALSE) {
+
+  if(!require(rvest) | !require(tidyr) | !require(dplyr)) {
+    stop("Precisa dos pacotes 'rvest', 'tidyr' e 'dplyr'.")
+  }
+  if (exportar == TRUE & arquivo == "") {
+    stop("Por favor, informe o nome do arquivo para que os dados sejam exportados")
+  }
+  if (silencioso == FALSE) {
+    cat("Realizando download dos dados: ")
+  }
 
   serie <- paste0("http://interwp.cepal.org/sisgen/ws/cepalstat/getDataWithoutMeta.asp?IdIndicator=",
                   id_serie, "&language=spanish")
@@ -26,10 +37,19 @@ CEPAL_stat <- function(id_serie) {
 
   n_dados <- pagina %>% xml_find_all("//info") %>% xml_attr('numeroDatos') %>% as.numeric()
 
-  if(nrow(dados) != n_dados |
-     length(dims) != n_dim) stop("Houve erro na compilação dos dados")
+  if(nrow(dados) != n_dados | length(dims) != n_dim) {
+    stop("Houve erro na compilação dos dados")
+  }
+
+  if (silencioso == FALSE) {
+    cat("Pronto!\n")
+  }
 
   # dicionario
+
+  if (silencioso == FALSE) {
+    cat("Realizando download do dicionário: ")
+  }
 
   url <- paste0("http://interwp.cepal.org/sisgen/ws/cepalstat/getDimensions.asp?idIndicator=",
                 id_serie,"&language=spanish")
@@ -56,6 +76,10 @@ CEPAL_stat <- function(id_serie) {
     id_dimensoes_lista[[i]] <- rep(id_dimensoes[i], times = netos[i])
   }; rm(i)
 
+  if (silencioso == FALSE) {
+    cat("Pronto!\n")
+  }
+
   dimencao <- do.call(c, id_dimensoes_lista)
 
   dicionario_ok <- cbind(dimencao, dicionario,
@@ -65,6 +89,9 @@ CEPAL_stat <- function(id_serie) {
   resultado <- dados
   names(resultado)[seq_len(n_dim)] <- nomes_dimensoes
 
+  if (silencioso == FALSE) {
+    cat("Unindo dados ao dicionário: ")
+  }
 
   for (i in seq_len(n_dim)) {
     dicionarios_lista[[i]] <- dicionario_ok %>%
@@ -73,14 +100,27 @@ CEPAL_stat <- function(id_serie) {
 
     names(dicionarios_lista[[i]])[1] <- names(resultado)[i]
 
-    resultado <- left_join(resultado, dicionarios_lista[[i]])
+    resultado <- left_join(resultado, dicionarios_lista[[i]],
+                           by = names(resultado)[i])
     names(resultado)[length(resultado)] <- paste0(names(resultado)[i], "_desc")
   }
 
   resultado$valor <- as.numeric(resultado$valor)
 
-  if ("Años_desc" %in% names(resultado))
+  if ("Años_desc" %in% names(resultado)) {
     resultado$Años_desc <- as.numeric(resultado$Años_desc)
+  }
+
+  if (silencioso == FALSE) {
+    cat("Pronto!\n")
+  }
+
+  if (exportar == TRUE) {
+    write.csv2(x = resultado, file = arquivo,
+               row.names = FALSE, fileEncoding = "UTF-8")
+
+    return(NULL)
+  }
 
   return(resultado)
 
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-RCEPAL
+# RCEPAL
+
+Este pacote contém funções para download dos dados disponíveis na API da [CEPALstat](http://estadisticas.cepal.org/cepalstat/WEB_CEPALSTAT/openDataAPI.asp?idioma=i).
+
+# Instalação
+
+```
+install.packages("devtools")
+devtools::install_github("tomasbarcellos/RCEPAL")
+library('RCEPAL')
+```
+# Exemplo
+
+```
+# Retorna data.frame com séries disponíveis na
+stat_disp <- series_CEPAL()
+
+# Retorna tabela com indicadores de termos de troca
+termos_troca <- CEPAL_stat(id_serie = 883)
+```
+
+# Contribuições
+
+Este trabalho ainda está em desenvolvimento. Caso encontre algum problema ou tenha alguma sugestão, faça-as [aqui](https://github.com/tomasbarcellos/RCEPAL/issues)
